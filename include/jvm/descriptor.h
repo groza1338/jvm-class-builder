504fe03ae733f6e289f63f0158e62c21654c56f3
KarbonST <mikhailbykadorov@mail.ru>
2026-01-10 21:44:39 +0300

    feat: introduce descriptor

---
+++ include/jvm/descriptor.h
@@ -0,0 +1,78 @@
+#ifndef JVM__DESCRIPTOR_H
+#define JVM__DESCRIPTOR_H
+
+#include <cstdint>
+#include <string>
+
+namespace Jvm
+{
+    class Descriptor
+    {
+    public:
+        /**
+         * Destructor.
+         */
+        virtual ~Descriptor() = default;
+
+        /**
+         * Primitive field types.
+         */
+        enum class PrimitiveFieldType
+        {
+            BYTE,
+            CHAR,
+            DOUBLE,
+            FLOAT,
+            INT,
+            LONG,
+            SHORT,
+            BOOLEAN,
+            UNKNOWN
+        };
+
+        /**
+         * @return descriptor by string
+         */
+        [[nodiscard]] virtual std::string toString() const;
+
+    protected:
+
+        /**
+        * struct of field type.
+        */
+        struct FieldType
+        {
+            PrimitiveFieldType primitiveFieldType_;
+            uint8_t arrayDepth_; // only for arrays
+            std::string classReference_; // only for class references
+        };
+
+        /**
+         * @param primitiveType  primitive field type.
+         * @return primitive field type by char.
+         */
+        static constexpr char primitiveTypeToChar(PrimitiveFieldType primitiveType)
+        {
+            switch (primitiveType)
+            {
+                case PrimitiveFieldType::BYTE: return 'B';
+                case PrimitiveFieldType::CHAR: return 'C';
+                case PrimitiveFieldType::DOUBLE: return 'D';
+                case PrimitiveFieldType::FLOAT: return 'F';
+                case PrimitiveFieldType::INT: return 'I';
+                case PrimitiveFieldType::LONG: return 'J';
+                case PrimitiveFieldType::SHORT: return 'S';
+                case PrimitiveFieldType::BOOLEAN: return 'Z';
+                case PrimitiveFieldType::UNKNOWN: return '_';
+                default: return '?';
+            }
+        }
+
+        static void isValidPrimitiveFieldType(PrimitiveFieldType primitiveFieldType);
+        static void isEmptyClassReference(const std::string& classReference);
+
+        FieldType fieldType_ = {PrimitiveFieldType::UNKNOWN, 0, ""};
+    };
+}
+
+#endif //JVM__DESCRIPTOR_H
---
+++ src/descriptor.cpp
@@ -0,0 +1,23 @@
+#include "jvm/descriptor.h"
+
+#include <stdexcept>
+
+using namespace Jvm;
+
+void Descriptor::isValidPrimitiveFieldType(PrimitiveFieldType primitiveFieldType)
+{
+    if (primitiveFieldType == PrimitiveFieldType::UNKNOWN)
+    {
+        throw std::invalid_argument("PrimitiveFieldType::UNKNOWN is not allowed");
+    }
+}
+
+void Descriptor::isEmptyClassReference(const std::string& classReference)
+{
+    if (classReference.empty())
+    {
+        throw std::invalid_argument(
+            "Class reference must not be empty"
+        );
+    }
+}
